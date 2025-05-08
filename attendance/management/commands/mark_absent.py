1d118483a251bc71ed291d7a7bd35762fe0d400e
NgKhao <bsbnk141@gmail.com>
2025-05-08 23:56:58 +0700

    fix: Inactive

--- attendance/management/commands/mark_absent.py
+++ attendance/management/commands/mark_absent.py
@@ -3,12 +3,13 @@ from django.core.management.base import BaseCommand
 from attendance.models import Attendance
 from employees.models import Employee
 
+
 class Command(BaseCommand):
-    help = 'Đánh dấu nhân viên vắng mặt nếu chưa check-in trước 10:00 sáng'
+    help = "Đánh dấu nhân viên vắng mặt nếu chưa check-in trước 10:00 sáng"
 
     def handle(self, *args, **kwargs):
         today = date.today()
-        all_employees = Employee.objects.all()
+        all_employees = Employee.objects.filter(status="Active")
         for employee in all_employees:
             if not Attendance.objects.filter(employeeId=employee, date=today).exists():
                 Attendance.objects.create(
@@ -16,4 +17,6 @@ class Command(BaseCommand):
                     date=today,
                     status="Absent",
                 )
-        self.stdout.write(self.style.SUCCESS('Đã đánh dấu vắng mặt cho các nhân viên chưa check-in.'))
+        self.stdout.write(
+            self.style.SUCCESS("Đã đánh dấu vắng mặt cho các nhân viên chưa check-in.")
+        )
